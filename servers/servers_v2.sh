b4c9389c4a872e6dc2b17000ea70fa4542d72a0a
Jeremy Varghese K <jeremyvk@Jeremys-MacBook-Pro.local>
2026-02-15 17:42:57 +0530

    Updated code to improve portability

--- servers/servers_v2.sh
+++ servers/servers_v2.sh
@@ -1,19 +1,21 @@
-#!/bin/bash/
+#!/usr/bin/env/ bash
+
+LLMS_ROOT="$HOME/llms"
 
 Ministral3() {
-  ~/llms/llama.cpp/build/bin/llama-server \
-    -m ~/llms/models/Ministral-3-8B-Instruct-2512-Q4_K_M.gguf \
+  "$LLMS_ROOT/llama.cpp/build/bin/llama-server" \
+    -m "$LLMS_ROOT/models/Ministral-3-8B-Instruct-2512-Q4_K_M.gguf" \
     --ctx-size 8192 \
     --threads 4 \
     --batch-size 256 \
     --host 0.0.0.0 \
-    --port 8080 
+    --port 8080
 }
 LLama3() {
-  ~/llms/llama.cpp/build/bin/llama-server \
-    -m ~/llms/models/Llama-3.2-1B-Instruct-Q4_K_M.gguf \
-  --ctx-size 8192 \
-  --threads 4 \
-  --batch-size 128 \
-  --port 8081
+  "$LLMS_ROOT/llama.cpp/build/bin/llama-server" \
+    -m "$LLMS_ROOT/models/Llama-3.2-1B-Instruct-Q4_K_M.gguf" \
+    --ctx-size 8192 \
+    --threads 4 \
+    --batch-size 128 \
+    --port 8081
 }
